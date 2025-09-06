b361dd7bb3582ed1af7eec7909b034e3263c1893
zmx-dream <61109333+zmx-dream@users.noreply.github.com>
2025-09-06 14:29:18 +0800

    Add files via upload

--- mnist_IPSG_AS.py
+++ mnist_IPSG_AS.py
@@ -1,5 +1,4 @@
 import copy
-import random
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,23 +8,26 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from mnist_optimer import *
 from mnist_model import *
-from mnist_function_gradient import *
 from torch.utils.data import DataLoader
 import time 
 
-# 定义训练设备, "cuda:0" 第一个GPU，也可以直接cuda
+# Define the training device, "cuda:0" is the first GPU, or you can use "cuda" directly
 device = torch.device("cuda")
-# 设置随机种子
-torch.manual_seed(42)  # 主要的随机种子
-torch.cuda.manual_seed_all(42)  # 如果使用CUDA，设置所有GPU的随机种子
+# Set the random seed
+torch.manual_seed(42)   # The main random seed
+torch.cuda.manual_seed_all(42)   # If using CUDA, set the random seed for all GPUs
 
-# 加载MNIST数据集
+# Load the MNIST dataset
 transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.1307,), (0.3081,))])
+                                 transforms.Normalize((0.1307,), (0.3081,))])
 
 train_data = torchvision.datasets.MNIST(root='D:/codes/Gradient_Sampling_LineSeach/GSL_NN/MNIST/datasets/mnist_dataset', train=True, download=True, transform=torchvision.transforms.ToTensor())
 
 test_data = torchvision.datasets.MNIST(root='D:/codes/Gradient_Sampling_LineSeach/GSL_NN/MNIST/datasets/mnist_dataset', train=False, download=True, transform=torchvision.transforms.ToTensor())
+
+# train_data = torchvision.datasets.FashionMNIST(root='D:/codes/Gradient_Sampling_LineSeach/GSL_NN/MNIST/datasets/mnist_dataset', train=True, download=True, transform=torchvision.transforms.ToTensor())
+
+# test_data = torchvision.datasets.FashionMNIST(root='D:/codes/Gradient_Sampling_LineSeach/GSL_NN/MNIST/datasets/mnist_dataset', train=False, download=True, transform=torchvision.transforms.ToTensor())
 
 train_images_list = []
 train_labels_list = []
@@ -37,32 +39,24 @@ for i in tqdm(range(len(train_data))):
 train_images_tensor = torch.stack(train_images_list).to(device)
 train_labels_tensor = torch.tensor(train_labels_list).to(device)
 
-# 长度
+# Length
 train_data_size = len(train_data)
 test_data_size = len(test_data)
-print("训练数据集的长度: {}".format(train_data_size))
-print("测试数据集的长度: {}".format(test_data_size))
+print("Training dataset length: {}".format(train_data_size))
+print("Test dataset length: {}".format(test_data_size))
 
-# # 假设我们想查看索引为 123 的图像
-# image_index = 123
 
-# # 获取图像和标签
-# image, label = train_data[image_index]
-
-# 实例化网络
+# Instantiate the network
 model = MNIST_Net()
 model_name = 'MNIST'
 data_name = 'mnist'
-# 网络模型转移到devic
+# Transfer the network model to the device
 model = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
 
-# 损失函数转移到cuda
+# Transfer the loss function to cuda
 criterion = criterion.to(device)
-# step_sizes1 = torch.arange(1, 5e-1,  -1e-1)
-# step_sizes1 = [1]
-# step_sizes2 = [1]
 
 step_sizes =  [2 ** (-2)]
 batch_sizes = [2**6]  
@@ -70,13 +64,13 @@ epss = [1000]
 gammas = [1e-3]
 betas = [0.25]
 
-# 保存最初的神经网络信息
+# Save the initial neural network information
 model_init_save = copy.deepcopy(model.state_dict())
 for beta in betas:
     for  lr_init in step_sizes:
         for  gamma in gammas:
             for  eps in epss:
-                # 每次训练之前初始化
+                # Initialize before each training session
                 model.load_state_dict(model_init_save)
                 opt = {
                     'train_data_size': train_data_size,
@@ -91,7 +85,7 @@ for beta in betas:
                     'eps_0': eps,
                     'gamma_1': gamma
                 }
-                # 结果保存
+                # Save results
                 result ={ 
                     'train_loss_val': [],
                     'test_loss_val': [],
@@ -103,51 +97,48 @@ for beta in betas:
                     'lr': [],
                     'nk': []
                 }
-                print('参数', opt)
+                print('Parameters', opt)
                 test_loss_min = np.Inf # track change in validation loss
 
-                # 参数的设置
-                # 正则项系数，类型
                 lambda_reg = opt['lambda_reg']
                 norm_q = opt['norm_q']
 
-                # 初始的batch_size
+                # Initial batch size
                 batch_size = opt['batch_size']
                 batch_size_init = batch_size
 
-                # 循环次数
+                # Number of epochs
                 n_epochs = opt['n_epochs']
 
-                # 步长以及最大步长
+                # Step size and maximum step size
                 lr_init = opt['lr_init']
                 lr_max = opt['lr_max']
 
-                # 动量系数
+                # Momentum coefficient
                 beta = opt['beta']
-                # 动态抽样系数
+                # Dynamic sampling coefficient
                 eps_0 = opt['eps_0']
                 gamma_1 = opt['gamma_1']
 
 
-
-                # 记录训练的次数
+                # Record the number of training iterations
                 total_train_step = 0
-                # 记录测试的次数
+                # Record the number of testing iterations
                 total_test_step = 0
 
 
-                # 训练的次数
+                # Number of training iterations
                 start_time = time.time()
                 batch_size_loader = batch_size
-                # 利用 dataloader 来加载数据集
+                # Use DataLoader to load the dataset
                 train_dataloader = DataLoader(train_data, batch_size=batch_size_loader)
                 test_dataloader = DataLoader(test_data, batch_size=batch_size_loader)
-                # 利用 dataloader 来加载数据集, 验证模型
+                # Use DataLoader to load the dataset for model validation
                 batch_size_loader_acc = 500
                 train_dataloader_acc = DataLoader(train_data, batch_size=batch_size_loader_acc)
                 test_dataloader_acc = DataLoader(test_data, batch_size=batch_size_loader_acc)
 
-                # 计算初始的函数值以及准确率
+                # Calculate the initial function value and accuracy
                 loss_reg = 0.0
                 train_loss = 0.0
                 test_loss = 0.0
@@ -165,16 +156,16 @@ for beta in betas:
                     loss = criterion(output, target)
                     # update average validation loss 
                     train_loss += loss.item()*data.size(0)
-                    # convert output probabilities to predicted class(将输出概率转换为预测类)
+                    # convert output probabilities to predicted class
                     _, pred = torch.max(output, 1)    
-                    # compare predictions to true label(将预测与真实标签进行比较)
+                    # compare predictions to true label
                     correct_tensor = pred.eq(target.data.view_as(pred))
                     # correct = np.squeeze(correct_tensor.to(device).numpy())
                     train_total_sample += batch_size_loader_acc
                     for i in correct_tensor:
                         if i:
                             train_right_sample += 1
-            
+                
                 for data, target in test_dataloader_acc:
                     data = data.to(device)
                     target = target.to(device)
@@ -184,9 +175,9 @@ for beta in betas:
                     loss = criterion(output, target)
                     # update average validation loss 
                     test_loss += loss.item()*data.size(0)
-                    # convert output probabilities to predicted class(将输出概率转换为预测类)
+                    # convert output probabilities to predicted class
                     _, pred = torch.max(output, 1)    
-                    # compare predictions to true label(将预测与真实标签进行比较)
+                    # compare predictions to true label
                     correct_tensor = pred.eq(target.data.view_as(pred))
                     # correct = np.squeeze(correct_tensor.to(device).numpy())
                     test_total_sample += batch_size_loader_acc
@@ -195,7 +186,7 @@ for beta in betas:
                             test_right_sample += 1
                 print("~Train_Accuracy:",100*train_right_sample/train_total_sample,"%", "Test_Accuracy:",100*test_right_sample/test_total_sample,"%")
 
-                # 计算平均损失
+                # Calculate average loss
                 if norm_q == 1:
                     for param in model.parameters():
                         loss_reg += torch.sum(torch.abs(param.data))
@@ -206,7 +197,7 @@ for beta in betas:
                 train_loss = train_loss/len(train_dataloader_acc.sampler) 
                 test_loss = test_loss/len(test_dataloader_acc.sampler)
 
-                # 显示训练集与验证集的损失函数与步长
+                # Display the loss and learning rate for the training and validation sets
                 print('Epoch: {} \tTraining Loss: {:.6f} \tTraining Loss+reg: {:.6f} \ttest Loss: {:.6f} \ttest Loss+reg: {:.6f} \tbatch_size: {:.6f},  \tlr: {:.6f}'.format(
                     0, train_loss, train_loss + lambda_reg * loss_reg.item(), test_loss, test_loss + lambda_reg * loss_reg.item(), batch_size, lr_init))
 
@@ -227,7 +218,7 @@ for beta in betas:
                 deleta_x = torch.zeros(1).to(device)
 
                 for epoch in tqdm(range(1, n_epochs+1)):
-                    print("--------第 {} 轮训练开始--------".format(epoch))
+                    print("--------Epoch {} training starts--------".format(epoch))
 
                 
 
@@ -240,12 +231,12 @@ for beta in betas:
                         for i, param in enumerate(model.parameters()):
                             param.data = param.data + beta * (param.data - x_old[i])
 
-                        # 计算在y_k处的梯度
+                        # Calculate the gradient at y_k
                         indices = torch.randint(0, train_data_size, (batch_size,), device=device)
                         stacked_images = train_images_tensor[indices]
                         stacked_targets = train_labels_tensor[indices]
                     
-                        # 原有代码保持不变
+                        # The original code remains unchanged
                         optimizer.zero_grad()
                         outputs = model(stacked_images)
                         loss = criterion(outputs, stacked_targets)
@@ -254,28 +245,28 @@ for beta in betas:
                         grad_y = [param.grad.data.clone() for param in model.parameters()]
 
                         x_old =  copy.deepcopy([param.data for param in model.parameters()])
-                        # x_{k+1} = prox(y_k - lr * grad(y_k))     
+                        # x_{k+1} = prox(y_k - lr * grad(y_k))    
                         for i, param in enumerate(model.parameters()):
                             param.data = param.data - lr * grad_y[i]
 
-                        # 近端算子更新
+                        # Proximal operator update
                         if norm_q == 1/2:
                             for i, param in enumerate(model.parameters()):
                                 # Compute threshold value
                                 t = (3/2) * (lr * lambda_reg)**(2/3)
-                                # 将大于t的进行变化
+                                # Change values greater than t
                                 mask = torch.abs(param.data) > t
                                 param_data_masked = param.data[mask]
                                 updated_values = (4/3) * param_data_masked * torch.cos(
                                     (torch.pi - torch.arccos((lr * lambda_reg / 4) * (3 / torch.abs(param_data_masked)).pow(3/2))) / 3
                                 ).pow(2)
                                 param.data[mask] = updated_values
-                                # 小于等于t的设置为0
+                                # Set values less than or equal to t to 0
                                 param.data = torch.where(torch.abs(param.data) <= t, torch.full_like(param.data, 0), param.data)
 
                         elif norm_q == 1:
                             for i, param in enumerate(model.parameters()):
-                                    param.data = torch.sign(param.data) * torch.max(torch.abs(param.data) - lambda_reg * lr, torch.zeros_like(param.data))
+                                     param.data = torch.sign(param.data) * torch.max(torch.abs(param.data) - lambda_reg * lr, torch.zeros_like(param.data))
 
                         if k % 100 == 0:
                             v_k = torch.zeros(1).to(device)
@@ -288,7 +279,6 @@ for beta in betas:
                             eps = eps_0 / ((k) ** (1.001)) 
                             delta = eps + gamma_1 * delta_x 
 
-                            # print('grad1', grads[-1], [param.data for param in MNIST_Net.parameters()][-1])
                             
                             for idx in range(len(stacked_targets)):
                                 image = stacked_images[idx].unsqueeze(0)
@@ -301,7 +291,7 @@ for beta in betas:
                                 for i, param in enumerate(model.parameters()):
                                     v_k += torch.sum(torch.mul(param.grad - grad_y[i], param.grad - grad_y[i]))
                     
-                            # print('grad2', [param.grad for param in MNIST_Net.parameters()][-1], [param.data for param in MNIST_Net.parameters()][-1])
+
                             v_k = v_k / (batch_size - 1)
 
                             if v_k * (train_data_size - batch_size) / (batch_size * (train_data_size - 1)) < delta:
@@ -311,9 +301,9 @@ for beta in betas:
                                 print(v_k, train_data_size, v_k_delta * train_data_size / (train_data_size - 1 + v_k_delta), batch_size + 1)
                                 batch_size = min(train_data_size, max(int(v_k_delta * train_data_size / (train_data_size - 1 + v_k_delta)), batch_size + 1)) 
                                 lr = min(lr/beta, lr_init * batch_size / batch_size_init, lr_max)
-                                # optimizer = SGD(model.parameters(),  lr = lr)
+
                         
-                    # 计算函数值以及准确率
+                    # Calculate function value and accuracy
                     loss_reg = 0.0
                     train_loss = 0.0
                     test_loss = 0.0
@@ -331,9 +321,9 @@ for beta in betas:
                         loss = criterion(output, target)
                         # update average validation loss 
                         train_loss += loss.item()*data.size(0)
-                        # convert output probabilities to predicted class(将输出概率转换为预测类)
+                        # convert output probabilities to predicted class
                         _, pred = torch.max(output, 1)    
-                        # compare predictions to true label(将预测与真实标签进行比较)
+                        # compare predictions to true label
                         correct_tensor = pred.eq(target.data.view_as(pred))
                         # correct = np.squeeze(correct_tensor.to(device).numpy())
                         train_total_sample += batch_size_loader_acc
@@ -350,9 +340,9 @@ for beta in betas:
                         loss = criterion(output, target)
                         # update average validation loss 
                         test_loss += loss.item()*data.size(0)
-                        # convert output probabilities to predicted class(将输出概率转换为预测类)
+                        # convert output probabilities to predicted class
                         _, pred = torch.max(output, 1)    
-                        # compare predictions to true label(将预测与真实标签进行比较)
+                        # compare predictions to true label
                         correct_tensor = pred.eq(target.data.view_as(pred))
                         # correct = np.squeeze(correct_tensor.to(device).numpy())
                         test_total_sample += batch_size_loader_acc
@@ -362,7 +352,7 @@ for beta in betas:
                     print("~Train_Accuracy:",100*train_right_sample/train_total_sample,"%", "Test_Accuracy:",100*test_right_sample/test_total_sample,"%")
 
 
-                    # 正则项部分损失
+                    # Regularization term loss
                     if norm_q == 1:
                         for param in model.parameters():
                             loss_reg += torch.sum(torch.abs(param.data))
@@ -373,7 +363,7 @@ for beta in betas:
                     train_loss = train_loss/len(train_dataloader_acc.sampler) 
                     test_loss = test_loss/len(test_dataloader_acc.sampler)
 
-                    # 显示训练集与验证集的损失函数与步长
+                    # Display the loss and learning rate for the training and validation sets
                     print('Epoch: {} \tTraining Loss: {:.6f} \tTraining Loss+reg: {:.6f} \ttest Loss: {:.6f} \ttest Loss+reg: {:.6f} \tbatch_size: {:.6f},  \tlr: {:.6f}'.format(
                         epoch, train_loss, train_loss + lambda_reg * loss_reg.item(), test_loss, test_loss + lambda_reg * loss_reg.item(), batch_size, lr))
                     
@@ -393,18 +383,9 @@ for beta in betas:
 
                     total_test_step = total_test_step + 1
 
-                    # # 如果验证集损失函数减少，就保存模型。
-                    # if test_loss <= test_loss_min:
-                    #     print('test loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(test_loss_min,test_loss))
-                    #     torch.save(model.state_dict(), 'checkpoint/resnet18_cifar10.pt')
-                    #     test_loss_min = test_loss
-                    #     counter = 0
-                    # else:
-                    #     counter += 1
 
-                with open('D:/codes/code_acc_KL/code/nonconvex_NN/result_mnist/result_mnist_Prox_IPSG.txt', 'a') as file:
+                with open('D:/codes/code_acc_KL/code/nonconvex_NN/result_mnist/result_mnist_Prox_IPSG_rand.txt', 'a') as file:
                     file.write(f"{'mnist_PPA_Swish'} Dataset:  {str(opt)} \n" + model_name + data_name)
                     file.write(str(result) + '\n')
                     file.write("=" * 40 + "\n")
                     file.close()
-
--- mnist_model.py
+++ mnist_model.py
@@ -1,45 +1,16 @@
-# 搭建神经网络, MNIST是个10分类的测试集
+
 import torch
 from torch import nn
-
-
-# class MNIST_Net(nn.Module):
-#     def __init__(self):
-#         super(MNIST_Net, self).__init__()
-#         self.fc1 = nn.Linear(28 * 28, 128)  # 28*28到128
-#         self.fc2 = nn.Linear(128, 64)    # 隐藏层到另一个隐藏层
-#         self.fc3 = nn.Linear(64, 10)     # 输出层，因为MNIST是十分类问题
-
-#     def forward(self, x):
-#         x = x.view(-1, 28 * 28)  # 将图像数据展平
-#         x = torch.relu(self.fc1(x))  
-#         x = torch.relu(self.fc2(x))  
-#         x = self.fc3(x)
-#         return x
-    
-# class MNIST_Net(nn.Module):
-#     def __init__(self):
-#         super(MNIST_Net, self).__init__()
-#         self.fc1 = nn.Linear(28 * 28, 128)  # 28*28到128
-#         self.fc2 = nn.Linear(128, 64)    # 隐藏层到另一个隐藏层
-#         self.fc3 = nn.Linear(64, 10)     # 输出层，因为MNIST是十分类问题
-
-#     def forward(self, x):
-#         x = x.view(-1, 28 * 28)  # 将图像数据展平
-#         x = torch.sigmoid(self.fc1(x))  
-#         x = torch.sigmoid(self.fc2(x))  
-#         x = self.fc3(x)
-#         return x
 
 class MNIST_Net(nn.Module):
     def __init__(self):
         super(MNIST_Net, self).__init__()
-        self.fc1 = nn.Linear(28 * 28, 128)  # 28*28到128
-        self.fc2 = nn.Linear(128, 64)    # 隐藏层到另一个隐藏层
-        self.fc3 = nn.Linear(64, 10)     # 输出层，因为MNIST是十分类问题
+        self.fc1 = nn.Linear(28 * 28, 128) 
+        self.fc2 = nn.Linear(128, 64)   
+        self.fc3 = nn.Linear(64, 10)     
 
     def forward(self, x):
-        x = x.view(-1, 28 * 28)  # 将图像数据展平
+        x = x.view(-1, 28 * 28)  
         x = self.fc1(x)
         x = x * torch.sigmoid(x)  
         x = self.fc2(x)
@@ -47,59 +18,10 @@ class MNIST_Net(nn.Module):
         x = self.fc3(x)
         return x
     
-
-class Swish(nn.Module):
-  def forward(self, input):
-    return (input * torch.sigmoid(input))
-  
-  def __repr__(self):
-    return self.__class__.__name__ + ' ()'
-
-class CNNModel(nn.Module):
-    def __init__ (self):
-        super(CNNModel,self).__init__()
-        
-        self.cnn1=nn.Conv2d(in_channels=1,out_channels=16, kernel_size=3,stride=1,padding=1)
-        self.bn1=nn.BatchNorm2d(16)
-        self.swish1=Swish()
-        nn.init.xavier_normal_(self.cnn1.weight)
-        self.maxpool1=nn.MaxPool2d(kernel_size=2,stride=1)
-        
-        self.cnn2=nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3,stride=1,padding=1)
-        self.bn2=nn.BatchNorm2d(32)
-        self.swish2=Swish()
-        nn.init.xavier_normal_(self.cnn2.weight)
-        self.maxpool2=nn.MaxPool2d(kernel_size=2)
-        
-        self.cnn3=nn.Conv2d(in_channels=32,out_channels=64, kernel_size=3,stride=1,padding=1)
-        self.bn3=nn.BatchNorm2d(64)
-        self.swish3=Swish()
-        nn.init.xavier_normal_(self.cnn3.weight)
-        self.maxpool3=nn.MaxPool2d(kernel_size=2)
-        self.fc1 = nn.Linear(64*6*6,10)
-        
-        
-    def forward(self,x):
-        out=self.cnn1(x)
-        out=self.bn1(out)
-        out=self.swish1(out)
-        out=self.maxpool1(out)
-        out=self.cnn2(out)
-        out=self.bn2(out)
-        out=self.swish2(out)
-        out=self.maxpool2(out)
-        out=self.cnn3(out)
-        out=self.bn3(out)
-        out=self.swish3(out)
-        out=self.maxpool3(out)
-        out=out.view(out.size(0),-1)
-        out=self.fc1(out)
-
-        return out
     
 # 验证网络
 if __name__ == '__main__':
-    MNIST_Net = CNNModel()
+    MNIST_Net = MNIST_Net()
     # 图片大小为28*28，64是一次训练的图片数量也就是minibatch=64
     input = torch.ones((1, 1, 28, 28))
     output = MNIST_Net(input)
